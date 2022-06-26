68a5eb16e5774eb3ff3f7a660e7d79f2d52943b1
ᴛғɴ ᴇᴍᴘɪʀᴇ <105818252+ITZRAJOP@users.noreply.github.com>
2022-06-26 06:37:56 +0530

    Update start.py

--- SemxXSpam/plugins/start.py
+++ SemxXSpam/plugins/start.py
@@ -4,11 +4,11 @@ from .. import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10, OWNER_ID
 from SemxXSpam.plugins.help import *
 
 
-RYAN_IMG = "https://te.legra.ph/file/91d82bf74b7a75468f5ef.jpg"
+RYAN_IMG = "https://telegra.ph/file/d23936efab1d2a0bacd54.jpg"
 
 Ryan_Button = [
         [
-        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/TheMKHackerX131")
+        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/TEAM_HEARTLESS_POLICE_ONLINE")
         ],
         [
         Button.inline("⚡ ᴄᴏᴍᴍᴀɴᴅs ⚡", data="help_back")
@@ -17,11 +17,11 @@ Ryan_Button = [
                
 RyanX_Button = [
         [
-        Button.url("✨ ᴄʜᴀɴɴᴇʟ ✨", "https://t.me/TheMKHackerX131"),
-        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/HACKERxSPAM")
+        Button.url("✨ ᴄʜᴀɴɴᴇʟ ✨", "https://t.me/ABOUT_STAR_BOY"),
+        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/TEAM_HEARTLESS_POLICE_ONLINE")
         ],
         [
-        Button.url("🔥 ʀᴇᴘᴏ 🔥", "https://github.com/ERR0rMK/SEMXxBOTFATHER")
+        Button.url("🔥 ʀᴇᴘᴏ 🔥", "https://github.com/ITZRAJOP/STARBOYxBOTFATHER")
         ]
         ]
         
